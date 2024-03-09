2880f7cf0ef83b1682dcf1108aa84297fbc4f167
Cristian Sterie <cristian.sterie@yahoo.com>
2024-03-09 19:12:01 +0200

    more random data;

--- src/main/java/ro/cristiansterie/vote/util/EntityHelper.java
+++ src/main/java/ro/cristiansterie/vote/util/EntityHelper.java
@@ -7,16 +7,21 @@ import java.util.Random;
 import ro.cristiansterie.vote.entity.CandidateDAO;
 import ro.cristiansterie.vote.entity.UserDAO;
 
-
 public class EntityHelper {
-    private static final String[] FIRST_NAMES = new String[] { "Mihai", "Andrei", "George", "Cristian", "Ion", "Paul",
-            "Ștefan", "Alexandru", "Petru", "Florin" };
-    private static final String[] LAST_NAMES = new String[] { "Popescu", "Ionescu", "Gabor", "Iliescu", "Petrescu",
-            "Chiricheș", "Protopopescu", "Deaconu", "Pamfil", "Pretop" };
+    private static final String[] FIRST_NAMES = new String[] { "Mihai", "Ana", "Andrei", "Adela", "George", "Andreea",
+            "Cristian", "Angela", "Ion", "Bianca", "Paul", "Ștefania", "Ștefan", "Anca", "Alexandru", "Vasilica",
+            "Petru", "Veta", "Florin" };
+    private static final String[] LAST_NAMES = new String[] { "Anghelescu", "Popescu", "Aftenie", "Ionescu",
+            "Adumitroaiei", "Dîrlău", "Gabor", "Decusară", "Iliescu", "Deju", "Petrescu", "Hârlișcă", "Chiricheș",
+            "Halbeș", "Kogălniceanu", "Kiriță", "Protopopescu", "Deaconu", "Pamfil", "Pretop", "Oală", "Similea" };
     private static final String[] PARTIES = new String[] { "PSD", "PNL", "USR", "UDMR", "PMP", "POT", "PD-L",
             "Noua Dreaptă", "Independent" };
 
     private static Random random = new Random();
+
+    private EntityHelper() {
+        // empty private constructor
+    }
 
     public static List<CandidateDAO> generateFakeCandidates(int no) {
         List<CandidateDAO> returnable = new ArrayList<>();
@@ -60,7 +65,8 @@ public class EntityHelper {
         UserDAO dto = new UserDAO();
 
         dto.setUsername(
-                FIRST_NAMES[random.nextInt(FIRST_NAMES.length)].toLowerCase() + "." + LAST_NAMES[random.nextInt(LAST_NAMES.length)].toLowerCase());
+                FIRST_NAMES[random.nextInt(FIRST_NAMES.length)].toLowerCase() + "."
+                        + LAST_NAMES[random.nextInt(LAST_NAMES.length)].toLowerCase());
 
         return dto;
     }
