60f1e8a630b58a799f6b6b0e80a22fa19a10b262
cristian.sterie <cristian.sterie@technohub.ro>
2025-03-02 15:50:34 +0200

    actually use H2 for testing;

--- src/test/java/ro/cristiansterie/vot/e/ApplicationTests.java
+++ src/test/java/ro/cristiansterie/vot/e/ApplicationTests.java
@@ -2,10 +2,12 @@ package ro.cristiansterie.vot.e;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import ro.cristiansterie.vote.VoteBackendApplication;
 
 @SpringBootTest(classes = VoteBackendApplication.class)
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
