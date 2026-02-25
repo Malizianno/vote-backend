83a4fb0eb94cbc04e9f67fcfa21b537f2414f427
cristian.sterie <cristian.sterie@technohub.ro>
2026-02-25 19:33:08 +0200

    fixing deploy build;

--- src/main/java/ro/cristiansterie/vote/config/DeepfaceProperties.java
+++ src/main/java/ro/cristiansterie/vote/config/DeepfaceProperties.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties(prefix = "deepface")
 public class DeepfaceProperties implements InitializingBean {
     protected static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
